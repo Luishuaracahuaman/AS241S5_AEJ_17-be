23762a32c7a9e65d36b7e141775aa82bfed215f5
Luishuaracahuaman <luis.huaraca.h@vallegrande.edu.pe>
2026-05-21 09:23:51 -0500

    chore: remove Maven wrapper files and project configuration

--- src/main/java/com/luisenrique/api_ia_mongodb/repository/IaResponseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.luisenrique.api_ia_mongodb.repository;
-
-import com.luisenrique.api_ia_mongodb.model.IaResponse;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IaResponseRepository extends ReactiveMongoRepository<IaResponse, String> {
-}
